b104d2c3cccb8ba736fcb162cdf05e61e089059b
Chad Dotson <chad@cdotson.com>
2018-07-08 20:46:48 -0500

    Completely working caching and server script.

--- downloader.py
+++ downloader.py
@@ -4,19 +4,20 @@ from hashlib import sha1 as sha
 from json import load, dump
 from logging import basicConfig, getLogger, DEBUG, INFO
 from os import error as oserror, makedirs
-from os.path import basename, exists, join
+from os.path import basename, dirname, exists, join
 from requests import get
 from semver import max_satisfying, lte
 from time import sleep
 from urllib import quote
 from urllib2 import urlopen
+import xml.etree.ElementTree as ET
 
 
 logger = getLogger(__name__)
 
 REPOSITORY_URL = 'https://registry.npmjs.org/'
 BUF_SIZE = 65536
-NICENESS = 0.01
+NICENESS = 0.001
 
 
 PACKAGES_NPM_REQUIRES=[
@@ -38,6 +39,11 @@ class PackageSpec(object):
         self.package_name = None
         self.package_version = None
         self._process_spec()
+
+    def __eq__(self, other):
+        # checking package spec is good enough since everything else is derived from it.
+        return self.package_spec == other.package_spec
+
 
     @property
     def registry_package_name(self):
@@ -71,9 +77,12 @@ class PackageSpec(object):
 
 def get_package_info(package):
     url = join(REPOSITORY_URL, package.replace('/', '%2f'))
-    logger.info("Getting info for %s from %s", package, url)
-
     content = get(url)
+    logger.info("Getting info for %s from %s, status: %d", package, url, content.status_code)
+
+    if content.status_code != 200:
+        raise FailedToDownloadPackageInfoError()
+
     return content.json()
 
 
@@ -89,6 +98,313 @@ def get_file_hash(path):
 
     return tmphash.hexdigest()
 
+
+# dict
+# - pkg
+# - - info
+# - - versions
+
+
+# class PackageVersionAlreadyRequiredError(RuntimeError):
+#     pass
+
+
+# class Package(object):
+#     def __init__(self, pkg_spec):
+#         self.registry_package_name = pkg_spec.registry_package_name
+#         self.is_scoped = pkg_spec.is_scoped
+#         self.scope = pkg_spec.scope
+#         self.scoped_package_name = pkg_spec.scoped_package_name
+#         self.package_name = pkg_spec.package_name
+#         self.package_version = pkg_spec.package_version
+
+#         self.info = get_package_info(pkg_spec.registry_package_name)
+#         self.required_versions = set()
+
+#     @property
+#     def latest(self):
+#         return str(self.info['dist-tags']['latest']) if self.info is not None else None
+
+#     @property
+#     def next_version(self):
+#         return str(self.info['dist-tags']['next']) if 'next' in self.info['dist-tags'] else None
+
+
+#     def add_required_version(self, pkg_spec):
+#         version_spec = pkg_spec.package_version
+#         logger.info('Initial version spec: %s', version_spec)
+
+#         if version_spec is None:
+#             logger.info("Version not specified, using latest (%s)", self.latest)
+#             version_spec =  self.latest
+
+#         # the python node-semver package doesn't work well with unicode, convert arguments to strings.
+#         pkg_versions = [str(v) for v in  self.info['versions'].keys()]
+#         filtered_pkg_versions = [v for v in pkg_versions if lte(v, self.latest, False)]
+
+#         logger.info('Finding version (%s) up to latest(%s)', version_spec, self.latest)
+
+#         download_version = max_satisfying(filtered_pkg_versions, str(version_spec))
+
+#         if download_version is None:
+#             logger.info('Version (%s) not found up to latest < latest(%s) trying all.', version_spec, self.latest)
+#             download_version = max_satisfying(pkg_versions, str(version_spec))
+        
+#         if download_version in self.required_versions:
+#             raise PackageVersionAlreadyRequiredError()
+        
+#         self.required_versions.add(download_version)
+#         return self.info['versions'][download_version]
+        
+
+class PackageVersionAlreadyRequiredError(RuntimeError):
+    pass
+
+
+class FailedToDownloadPackageInfoError(RuntimeError):
+    pass
+
+class Package(object):
+    def __init__(self, pkg_name):
+        self.pkg_name = pkg_name
+        self.is_scoped = pkg_name.startswith('@')
+
+        self.info = get_package_info(self.pkg_name)
+        self.required_versions = set()
+
+    @property
+    def latest(self):
+        return str(self.info['dist-tags']['latest']) if self.info is not None else None
+
+    @property
+    def next_version(self):
+        return str(self.info['dist-tags']['next']) if 'next' in self.info['dist-tags'] else None
+
+
+    def add_required_version(self, pkg_version):
+        version_spec = pkg_version
+        logger.debug('Initial version spec: %s', version_spec)
+
+        if version_spec is None:
+            logger.debug("Version not specified, using latest (%s)", self.latest)
+            version_spec =  self.latest
+
+        # the python node-semver package doesn't work well with unicode, convert arguments to strings.
+        pkg_versions = [str(v) for v in  self.info['versions'].keys()]
+        filtered_pkg_versions = [v for v in pkg_versions if lte(v, self.latest, False)]
+
+        logger.debug('Finding version (%s) up to latest(%s)', version_spec, self.latest)
+
+        download_version = max_satisfying(filtered_pkg_versions, str(version_spec))
+
+        if download_version is None:
+            logger.debug('Version (%s) not found up to latest < latest(%s) trying all.', version_spec, self.latest)
+            download_version = max_satisfying(pkg_versions, str(version_spec))
+        
+        if download_version in self.required_versions:
+            raise PackageVersionAlreadyRequiredError()
+        
+        self.required_versions.add(download_version)
+        return self.info['versions'][download_version]
+    
+
+
+def determine_download_version(pkg_spec, package):
+    version_spec = pkg_spec.package_version
+    logger.info('Initial version spec: %s', version_spec)
+
+    if version_spec is None:
+        logger.info("Version not specified, using latest (%s)", package.latest)
+        version_spec =  package.latest
+
+    # the python node-semver package doesn't work well with unicode, convert arguments to strings.
+    pkg_versions = [str(v) for v in  package.info['versions'].keys()]
+    filtered_pkg_versions = [v for v in pkg_versions if lte(v, package.latest, False)]
+
+    logger.info('Finding version (%s) up to latest(%s)', version_spec, package.latest)
+
+    download_version = max_satisfying(filtered_pkg_versions, str(version_spec))
+
+    if download_version is None:
+        logger.info('Version (%s) not found up to latest < latest(%s) trying all.', version_spec, package.latest)
+        download_version = max_satisfying(pkg_versions, str(version_spec))
+    
+    return download_version
+
+
+
+def split_package_spec(spec):
+    if spec.startswith('@'):
+        return spec.rsplit('@', 1)  if spec.count('@') > 1 else (spec, None)
+    else:
+        return spec.rsplit('@', 1) if '@' in spec else (spec, None)
+    
+
+def crawl_package_info(packages, spec):
+    """ Download the package specified into the output directory specified. """
+
+    logger.debug('Processing %s', spec)
+
+    pkg_name, pkg_version = split_package_spec(spec)
+
+    if pkg_name not in packages:
+        try:
+            packages[pkg_name] = Package(pkg_name)
+        except FailedToDownloadPackageInfoError:
+            logger.exception('Failed to download packge info for: %s', pkg_name)
+            return
+
+    package = packages[pkg_name]
+    
+    try:
+        version_info = package.add_required_version(pkg_version)
+        
+        # cache all package dependencies and optoinal dependencies.
+        logger.debug('Processing dependencies: %s', pkg_name)
+        for dependency, version in version_info.get('dependencies', {}).items():
+            crawl_package_info(packages, dependency + '@' + version)
+    
+        logger.debug('Processing optional dependencies: %s', pkg_name)
+        for dependency, version in version_info.get('dependencies', {}).items():
+            crawl_package_info(packages, dependency + '@' + version)
+        
+    except PackageVersionAlreadyRequiredError:
+        logger.debug('Package version already required - package: %s, version %s', pkg_name, pkg_version)
+        pass
+
+    logger.debug('Done with: %s', spec)
+    sleep(NICENESS)
+
+
+
+# def crawl_package_info(packages, spec):
+#     """ Download the package specified into the output directory specified. """
+
+#     logger.info('Processing %s', spec)
+
+#     pkg_spec = PackageSpec(spec)
+
+
+
+
+#     if pkg_spec.registry_package_name not in packages:
+#         packages[pkg_spec.registry_package_name] = Package(pkg_spec)
+        
+#     package = packages[pkg_spec.registry_package_name]
+    
+#     try:
+#         version_info = package.add_required_version(pkg_spec)
+        
+#         # cache all package dependencies and optoinal dependencies.
+#         logger.info('Processing dependencies: %s', pkg_spec.registry_package_name)
+#         for dependency, version in version_info.get('dependencies', {}).items():
+#             crawl_package_info(packages, dependency + '@' + version)
+    
+#         logger.info('Processing optional dependencies: %s', pkg_spec.registry_package_name)
+#         for dependency, version in version_info.get('dependencies', {}).items():
+#             crawl_package_info(packages, dependency + '@' + version)
+        
+#     except PackageVersionAlreadyRequiredError:
+#         logger.info('Package version already required - package: %s, version %s', pkg_spec.registry_package_name, pkg_spec)
+#         pass
+
+#     logger.info('Done with package: %s', pkg_spec.registry_package_name)
+#     sleep(NICENESS)
+
+
+
+def download_chromedriver(output_directory):
+    logger.info('Downloading chromedriver resources.')
+    ns = {'ChromeDriver': 'http://doc.s3.amazonaws.com/2006-03-01'}
+
+    r = get('http://chromedriver.storage.googleapis.com/')
+
+    root = ET.fromstring(r.content)
+
+    info_output_file = join(output_directory, 'chromedriver.xml')
+    resource_base = output_directory
+
+    try:
+        makedirs(resource_base)
+    except oserror:
+        pass
+        # logger.exception("Error creating output directory for chrome driver.")
+
+    chromedriver_info = get('https://chromedriver.storage.googleapis.com/')
+
+    with open(info_output_file, 'w') as f:
+        print(chromedriver_info.content, file=f)
+        
+    for installable in root.findall('ChromeDriver:Contents', ns):
+        element = installable.find('ChromeDriver:Key', ns)
+        resource_url = join('https://chromedriver.storage.googleapis.com/', element.text)
+        
+        save_path = join(resource_base, element.text)
+        try:
+            makedirs(dirname(save_path))
+        except oserror:
+            pass
+            # logger.exception("Error creating output directory for chrome driver version.")
+
+
+        if exists(save_path):
+            logger.info("Using cached %s at %s", resource_url, save_path)
+            continue
+
+        logger.info("Downloading %s to %s", resource_url, save_path)
+        
+        downloaded = get(resource_url)
+
+        with open(save_path, 'wb') as f:
+            f.write(downloaded.content)
+
+def download_node_dependencies(output_directory_base, specified_packages):
+    logger.info('Downloading node dependencies.')
+
+    required_packages = {}
+    for spec in packages:
+        crawl_package_info(required_packages, spec)
+
+    for package in required_packages.values():
+        if package.is_scoped:
+            scope, file_base = package.pkg_name.split('/', 1)
+            output_directory = join(output_directory_base, scope)
+        else:
+            file_base = package.pkg_name
+            output_directory = output_directory_base
+                    
+        info_path = join(output_directory, file_base) + '.json'
+        tgz_directory = join(output_directory, 'tgz')
+
+        try:
+            if not exists(tgz_directory):
+                makedirs(tgz_directory)
+        except oserror:
+            logger.exception("Error creating output directory.")
+
+        with open(info_path, 'w') as f:
+            dump(package.info, f, indent=4)
+
+        for version in package.required_versions:
+            tarball_url = package.info['versions'][version]['dist']['tarball']
+            expected_shasum = package.info['versions'][version]['dist']['shasum']
+            tgz_path = join(tgz_directory, basename(tarball_url))
+
+            # print(package.pkg_name, version, tarball_url, expected_shasum)
+
+            if exists(tgz_path) and get_file_hash(tgz_path) == expected_shasum:
+                logger.info('Locally cached package: %s version: %s (latest: %s) at: %s', package.pkg_name, version, package.latest, tarball_url)
+            else:
+                logger.info('Getting package: %s version: %s (latest: %s) at: %s', package.pkg_name, version, package.latest, tarball_url)
+
+                tarball = get(tarball_url)
+                with open(tgz_path, 'wb') as f:
+                    f.write(tarball.content)
+
+                file_shasum = get_file_hash(tgz_path) if exists(tgz_path) else None
+
+                if not file_shasum == expected_shasum:
+                    logger.warning('''Package %s from %s downloaded by hash: %s doesn't match expected hash: %s''', package.pkg_name, tarball_url, file_shasum, expected_shasum)
 
 
 
@@ -152,7 +468,6 @@ def download_package(output_directory, spec, duplicate_download_preventer, force
     except oserror:
         logger.exception("Error creating output directory.")
 
-
     with open(info_path, 'w') as f:
         dump(info, f, indent=4)
 
@@ -197,6 +512,8 @@ def get_args():
     parser = ArgumentParser(description='Image Scraper')
     parser.add_argument('output_directory', help='Output file')
     parser.add_argument('packages', type=str, nargs='+', help='packages to cache (space seperated)')
+    parser.add_argument('--skip_chromedriver', help='Don\'t download chromedriver files.', default=False, action='store_true' )
+    parser.add_argument('--skip_node', help='Don\'t download node dependencies.', default=False, action='store_true' )
     parser.add_argument('-p', '--package', help='packages argument is package.json formatted file.', default=False, action='store_true' )
     parser.add_argument('-n', '--noextras', help='don\'t include node extras (dependencies automatically gotten by node.', default=False, action='store_true' )
     parser.add_argument('-v', '--verbose', help='Verbose log output', default=False, action='store_true')
@@ -210,26 +527,23 @@ if __name__ == '__main__':
     if args.verbose:
         getLogger('').setLevel(DEBUG)
 
-    # simple method for preventing circular dependencies.  I don't know if that is
-    # possible but I seem to have hit something like it.
-    duplicate_download_preventer = dict()
+    if not args.skip_chromedriver:
+        download_chromedriver(join(args.output_directory, 'chromedriver'))
+    
+    if not args.skip_node:
+        # if -p is specified, that means that instead of a list of packages via the command line,
+        # a package.json format file has been specified.  We want to cache everything about it.
+        packages = []
+        if args.package:
+            with open(args.packages[0], 'r') as f:
+                info = load(f)
+                packages += [dependency + '@' + version for dependency, version in info.get('devDependencies', {}).items()]
+                packages += [dependency + '@' + version for dependency, version in info.get('dependencies', {}).items()]
+                packages += [dependency + '@' + version for dependency, version in info.get('optionalDependencies', {}).items()]
+        else:
+            packages = args.packages
 
-    # if -p is specified, that means that instead of a list of packages via the command line,
-    # a package.json format file has been specified.  We want to cache everything about it.
-    packages = []
-    if args.package:
-        with open(args.packages[0], 'r') as f:
-            info = load(f)
-            packages += [dependency + '@' + version for dependency, version in info.get('devDependencies', {}).items()]
-            packages += [dependency + '@' + version for dependency, version in info.get('dependencies', {}).items()]
-            packages += [dependency + '@' + version for dependency, version in info.get('optionalDependencies', {}).items()]
-    else:
-        packages = args.packages
+        packages = packages if args.noextras else PACKAGES_NPM_REQUIRES + packages
 
-
-    for package in packages if args.noextras else PACKAGES_NPM_REQUIRES + packages:
-    #for package in packages:
-        download_package(args.output_directory, package, duplicate_download_preventer)
-
-    logger.info('Downloaded %d packages total.', sum(len(s) for s in duplicate_download_preventer.values()))
+        download_node_dependencies(join(args.output_directory, 'node'), packages)
 
--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 from __future__ import print_function
+from argparse import ArgumentParser
 from flask import Flask, jsonify, request, send_from_directory
 from json import load
 from logging.config import dictConfig
 from os.path import join
+from socket import gethostname
+from urllib import unquote
 
 app = Flask(__name__)
 
-
+# examples of non-scoped and scoped urls
 # https://registry.npmjs.org/vue
 # https://registry.npmjs.org/vue/-/vue-2.5.16.tgz
 
@@ -38,37 +41,47 @@ def load_json_info(path_to_json):
     if u'versions' in content:
         for version in content['versions'].values():
             if u'dist' in version and u'tarball' in version['dist']:
-                version['dist']['tarball']= version['dist']['tarball'].replace('https://registry.npmjs.org/', 'http://localhost:5000/')
+                version['dist']['tarball']= version['dist']['tarball'].replace('https://registry.npmjs.org/', app.config['NODE_URL'])
+                version['dist']['tarball']= version['dist']['tarball'].replace('http://registry.npmjs.org/', app.config['NODE_URL'])
     return content
 
 
-@app.route('/<string:package>')
+@app.route('/node/<string:package>')
 def get_package_info(package):
+    package = unquote(package)
     app.logger.info("Getting: %s", package)
-    path_to_json = join('./npm', package) + '.json'
+    path_to_json = join(app.config['NODE_CACHE_DIRECTORY'], package) + '.json'
     info = load_json_info(path_to_json)
     return jsonify(info)
 
 
-@app.route('/@<string:scope>/<string:package>')
+@app.route('/node/<string:scope>/<string:package>')
 def get_scoped_package_info(scope, package):
     app.logger.info("Getting: @%s/%s", scope, package)
-    path_to_json = join('./npm', 'scoped', scope, package) + '.json'
+    path_to_json = join(app.config['NODE_CACHE_DIRECTORY'], scope, package) + '.json'
     info = load_json_info(path_to_json)
     return jsonify(info)
 
 
-@app.route('/<string:package>/-/<string:tarball>')
+@app.route('/node/<string:package>/-/<string:tarball>')
 def get_package_tgz(package, tarball):
-    path_to_tarballs = join('./npm', 'tgz')
+    package = unquote(package)
+    path_to_tarballs = join(app.config['NODE_CACHE_DIRECTORY'], 'tgz')
+    app.logger.info('getting package %s from %s', package, path_to_tarballs)
     return send_from_directory(path_to_tarballs, tarball)
 
 
-@app.route('/@<string:scope>/<string:package>/-/<string:tarball>')
+@app.route('/node/<string:scope>/<string:package>/-/<string:tarball>')
 def get_scoped_package_tgz(scope, package, tarball):
-    path_to_tarballs = join('./npm', 'scoped', scope,  'tgz')
+    path_to_tarballs = join(app.config['NODE_CACHE_DIRECTORY'], scope,  'tgz')
 
     return send_from_directory(path_to_tarballs, tarball)
+
+
+@app.route('/chromedriver/<path:path>')
+def get_chromedriver_file(path):
+    return send_from_directory(app.config['CHROMEDRIVER_CACHE_DIRECTORY'], path)
+
 
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
@@ -81,3 +94,46 @@ def shutdown_server():
 def shutdown():
     shutdown_server()
     return 'Server shutting down...'
+
+
+def get_args():
+    parser = ArgumentParser(description='Offline Server (node, chromedriver, etc)')
+    parser.add_argument('cache_directory', help='Cache directory')
+    parser.add_argument('--host', help='Host', default='0.0.0.0', type=str)
+    parser.add_argument('--port', help='Port', default=16000, type=int)
+    parser.add_argument('-v', '--verbose', help='Verbose log output', default=False, action='store_true')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    #app.run(ssl_context='adhoc')
+
+    args = get_args()
+    app.config['CACHE_DIRECTORY'] = args.cache_directory
+    app.config['NODE_CACHE_DIRECTORY'] = join(args.cache_directory, 'node')
+    app.config['CHROMEDRIVER_CACHE_DIRECTORY'] = join(args.cache_directory, 'chromedriver')
+    app.config['HOST'] = args.host
+    app.config['PORT'] = args.port
+    app.config['NODE_URL'] = new_url = '{0}://{1}:{2}/node/'.format('http', app.config['HOST'], app.config['PORT'])
+    app.config['CHROMEDRIVER_URL'] = new_url = '{0}://{1}:{2}/chromedriver/'.format('http', app.config['HOST'], app.config['PORT'])
+
+
+    app.logger.info('Serving directory: %s', app.config['CACHE_DIRECTORY'])
+    app.logger.info('Server url: %s', app.config['NODE_URL'])
+
+    app.logger.info('\n\n***** NOTES *****\n* npm audit is not currently supported.\n* Add the following to a relevant .npmrc file.\n' \
+                    '--------------------------------------\n' \
+                    'registry=%s\naudit=false\n' \
+                    '--------------------------------------\n\n' \
+                    '* Add the following environment variable.\n' \
+                    '--------------------------------------\n' \
+                    'export CHROMEDRIVER_CDNURL=%s\n' \
+                    '--------------------------------------\n',
+                    app.config['NODE_URL'],
+                    app.config['CHROMEDRIVER_URL'])
+    
+    app.run(
+        debug=args.verbose,
+        host=args.host,
+        port=args.port,
+    )
